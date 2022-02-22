9931888e31fcd361415f4dc7357479ad606b4fd6
okeikei <kkujimp@gmail.com>
2022-02-22 14:09:31 +0000

    [Fix] customers_controller.rb

--- app/controllers/customer/customers_controller.rb
+++ app/controllers/customer/customers_controller.rb
@@ -1,51 +1,51 @@
 class Customer::CustomersController < ApplicationController
   before_action :authenticate_customer!
-  
+
   def show
     @customer = current_customer
   end
-    
+
   def index
     @customers = Customer.all
-    @customers = Customer.all.order(rank_point: :desc)
+    # @customers = Customer.all.order(rank_point: :desc)
     @sell_items = current_customer.items
   end
-  
+
   def unsubscribe
     @customer = current_customer
   end
-  
+
   def register
     @customer = Customer.find(params[:id])
     @sell_items = @customer.items
   end
-  
+
   def follow
     @customer = Customer.find(params[:id])
     @customers = @customer.followings
   end
-  
+
   def follower
     @customer = Customer.find(params[:id])
     @customers = @customer.followers
   end
-  
+
   def favorite
     @item = Item.find(params[:id])
     #@customers = @item.favorites
     @favorites = @item.favorites
   end
-  
+
   def edit
     @customer = current_customer
   end
-  
+
   def update
     @customer = current_customer
     @customer.update(customer_params)
     redirect_to customer_show_path
   end
-  
+
   def withdraw
     current_customer.destroy
     reset_session
@@ -58,7 +58,7 @@ class Customer::CustomersController < ApplicationController
   def customer_params
     params.require(:customer).permit(:last_name, :first_name, :last_name_kana, :first_name_kana, :postal_code, :address, :telephone_number, :image)
   end
-  
+
   def reject_customer
     @customer = Customer.find_by(email: params[:customer][:email])
     if @customer
@@ -70,5 +70,5 @@ class Customer::CustomersController < ApplicationController
       end
     end
   end
-  
+
 end
